13b52ca03a58e4681e2cd3a3f91a229bcf7435eb
Ravan Naidoo <rnaidoo@xebialabs.com>
2016-11-24 12:23:46 +0100

    Refactored upload retry logic for poll for a minute

--- src/main/jython/azure_app_services/upload_web_app_artifact.py
+++ src/main/jython/azure_app_services/upload_web_app_artifact.py
@@ -5,15 +5,13 @@ import time
 def upload(deployed, container):
     client = AzureClient.new_instance(container.subscription)
     retry_count = 0
-    while retry_count < 10:
+    while retry_count < 12:
         print "Checking if Azure ftp upload service available for web site [%s]" % deployed.appName
-        service_available = client.is_kudu_services_available(deployed.appName)
-        if service_available:
+        if client.is_kudu_services_available(deployed.appName):
             break
-        else:
-            retry_count += 1
-            print "Failed to connect to Azure ftp upload service. Will retry in 5 seconds. Attempt %s" % retry_count
-            time.sleep(5)
+        retry_count += 1
+        print "Failed to connect to Azure ftp upload service. Will retry in 5 seconds. Attempt %s" % retry_count
+        time.sleep(5)
 
     print "Stopping website if already running. This to prevent file locking."
     client.stop_website(container.resourceName, deployed.appName)
